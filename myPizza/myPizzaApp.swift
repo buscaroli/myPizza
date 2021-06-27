cc4ad0ebc5145dce9007def759e5514c0c13f2af
mabus <m.buscaroli@outlook.com>
2021-06-27 10:58:11 +0100

    Initial Commit

---
+++ myPizza/myPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPizzaApp.swift
+//  myPizza
+//
+//  Created by Matteo on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct myPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
